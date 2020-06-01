373459a910415c40f7fbe0aadd9945205d68bd6c
Artyom Koshko <androidakbluebox@gmail.com>
2020-06-02 00:31:15 +0300

    Add language switcher, add project cards

--- ashchuk/src/app/app.component.ts
+++ ashchuk/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TranslocoService} from '@ngneat/transloco';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'ashchuk';
+  title = 'ashchuk🇺🇸🇬🇧🇫🇷';
+
+  // the locales the app supports
+  locales = [
+    { label: '🇬🇧 English', value: 'en-GB' },
+    { label: '🇷🇺 Русский', value: 'ru-RU' }
+  ];
+  // the user's locale
+  detectedLocale = '';
+  // the default locale
+  locale = this.locales[0].value;
+
+  constructor(private translocoService: TranslocoService) {
+    this.detectedLocale = this.getUsersLocale('en-GB');
+
+    // generate a regex from the locales we support
+    const supportedRegex = new RegExp('^' + this.locales.map(l => l.value.substring(0, 2)).join('|^'));
+    // check if the user's preferred language is supported and if so, use it.
+    if (this.detectedLocale.match(supportedRegex)) {
+      this.updateLocale(this.detectedLocale);
+    }
+  }
+
+  getUsersLocale(defaultValue: string): string {
+    if (typeof window === 'undefined' || typeof window.navigator === 'undefined') {
+      return defaultValue;
+    }
+    const wn = window.navigator as any;
+    let lang = wn.languages ? wn.languages[0] : defaultValue;
+    lang = lang || wn.language || wn.browserLanguage || wn.userLanguage;
+    return lang;
+  }
+  // change locale/language at runtime
+  updateLocale(locale) {
+    console.log('update locale', locale);
+    if (this.locales.some(l => l.value === locale)) {
+      this.locale = locale;
+    }
+    const lang = locale.substring(0, 2);
+    this.translocoService.setActiveLang(lang);
+  }
 }
--- ashchuk/src/app/app.module.ts
+++ ashchuk/src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -14,10 +21,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+    TranslocoRootModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
